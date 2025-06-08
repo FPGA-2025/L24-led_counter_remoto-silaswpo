2252202074ab5378e7ac361426d9d8d606b06e64
silaswpo <silaswesleypereiradeoliveira@gmail.com>
2025-06-08 03:18:15 -0300

    somente 1 deu certo

--- led_counter.v
+++ led_counter.v
@@ -1,10 +1,20 @@
 module Counter #(
-    parameter CLK_FREQ = 25_000_000 
-) (
+    parameter CLK_FREQ = 25_000_000  // ignorado pelo testbench
+)(
     input  wire clk,
     input  wire rst_n,
-    output wire [7:0] leds
+    output reg [7:0] leds
 );
 
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            leds <= 0;
+        end else begin
+            if (leds == 7)
+                leds <= 0;
+            else
+                leds <= leds + 1;
+        end
+    end
 
 endmodule
